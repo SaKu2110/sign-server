06b34666c46ab49df87e41ef3fabaa9047443ead
SaKu2110 <b1018106@fun.ac.jp>
2019-06-20 08:46:07 +0000

    環境変数からデータを取ってくるようにした

--- service/database/attempt.go
+++ service/database/attempt.go
@@ -3,14 +3,15 @@ package database
 import(
 	"github.com/jinzhu/gorm"
 	"context"
+	"os"
 )
 
 func attempt (sql *DB,ctx context.Context) {
 	sql.DBMS	= "mysql"
-	sql.USER	= "keeper"
-	sql.PASS	= "admin_keeper"
-	sql.PROTOCOL	= "tcp(127.0.0.1:3306)"
-	sql.DBNAME	= "sign"
+	sql.USER	= os.Getenv("DB_USER")
+	sql.PASS	= os.Getenv("DB_PASS")
+	sql.PROTOCOL	= os.Getenv("DB_IP")
+	sql.DBNAME	= os.Getenv("DB_NAME")
 	sql.CONNECT	= sql.USER+":"+sql.PASS+"@"+sql.PROTOCOL+"/"+sql.DBNAME
 
 	sql.DB, sql.ERROR = gorm.Open(sql.DBMS, sql.CONNECT)
