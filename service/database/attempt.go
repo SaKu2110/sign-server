b67d168feb870ed9ea1c0bd244835a5e8aa073f4
SaKu2110 <b1018106@fun.ac.jp>
2019-06-20 14:40:13 +0000

    mysqlにコンタクト時のオプションを追加

--- service/database/attempt.go
+++ service/database/attempt.go
@@ -13,7 +13,7 @@ func attempt (sql *DB,ctx context.Context) {
 	sql.PROTOCOL	= os.Getenv("DB_IP")
 	sql.PORT	= os.Getenv("DB_PORT")
 	sql.DBNAME	= os.Getenv("DB_NAME")
-	sql.CONNECT	= sql.USER+":"+sql.PASS+"@tcp("+sql.PROTOCOL+":"+sql.PORT+")"+"/"+sql.DBNAME
+	sql.CONNECT	= sql.USER+":"+sql.PASS+"@tcp("+sql.PROTOCOL+":"+sql.PORT+")"+"/"+sql.DBNAME+"?charset=utf8&parseTime=True&loc=Local"
 
 	sql.DB, sql.ERROR = gorm.Open(sql.DBMS, sql.CONNECT)
 }
