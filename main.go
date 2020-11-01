91d125fde8068fe7e98c830724ca463ae0b07013
SaKu2110 <b1018106@fun.ac.jp>
2020-11-01 23:56:18 +0900

    [Upadet] main package

--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package main
 import(
 	"log"
 	"github.com/SaKu2110/sign-server/pkg/server"
+	"github.com/SaKu2110/sign-server/pkg/model/dao"
 	"github.com/SaKu2110/sign-server/pkg/controller"
 )
 
 func main() {
-	ctrl := controller.Init()
-	if err := server.Router(ctrl).Run(); err != nil {
+	db, err := dao.Init()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	ctrl := controller.Init(db)
+	if err := server.Router(ctrl).Run(":9000"); err != nil {
 		log.Fatal(err)
 	}
 }
