ada039fd291f3318f24cbb1c826d0e6eb46c5f89
SaKu2110 <b1018106@fun.ac.jp>
2020-11-01 19:27:50 +0900

    [Create] skeleton code

--- main.go
+++ main.go
@@ -1,36 +1,14 @@
 package main
 
 import(
-	"github.com/SaKu2110/sign-server/service/database"
-	"github.com/SaKu2110/sign-server/controllers"
-	"github.com/gin-gonic/gin"
 	"log"
+	"github.com/SaKu2110/sign-server/pkg/server"
+	"github.com/SaKu2110/sign-server/pkg/controller"
 )
-
-var(
-	SQL = &database.DB{}
-	CCH = &controller.CCH{}
-)
-
-func init() {
-	SQL.Connect()
-	// タイムアウト処理
-	if SQL.ERROR != nil {
-		log.Printf(SQL.MSG)
-		panic(SQL.ERROR.Error())
-	}
-	// ハンドラ内でDBの操作ができるようにする
-	CCH.DB = SQL.DB
-}
 
 func main() {
-	// Ginサーバの起動
-	gin := gin.Default()
-	gin.GET("/ping", CCH.PingHandler)
-	gin.POST("/signin", CCH.SignInHandler)
-	gin.POST("/signup", CCH.SignUpHandler)
-	if CCH.ERROR != nil {
-		log.Fatal(CCH.ERROR)
+	ctrl := controller.Init()
+	if err := server.Router(ctrl).Run(); err != nil {
+		log.Fatal(err)
 	}
-	gin.Run()
 }
