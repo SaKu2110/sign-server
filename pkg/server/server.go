640524a4eb2fd7c03a836a941a7ca1cb9d45a6c4
SaKu2110 <b1018106@fun.ac.jp>
2020-11-02 01:11:03 +0900

    [Add] pinh handler

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,13 +1,17 @@
 package server
 
 import(
+	"net/http"
 	"github.com/gin-gonic/gin"
 	"github.com/SaKu2110/sign-server/pkg/controller"
 )
 
 func Router(ctrl controller.Controller) (router *gin.Engine) {
 	router = gin.Default()
-	router.GET("/ping", ctrl.PingHandler)
+	// handler作るのめんどくさかった...
+	router.GET("/ping", func(cxt *gin.Context) {
+        cxt.JSON(http.StatusOK, gin.H{"message": "pong"})
+    })
 	router.POST("/signin", ctrl.SignInHandler)
 	router.POST("/signup", ctrl.SignUpHandler)
 	return
