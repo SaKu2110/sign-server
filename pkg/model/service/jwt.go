39db10f88a02b71e35ef3e9acb2b348aa39c61c9
SaKu2110 <b1018106@fun.ac.jp>
2020-11-02 01:03:47 +0900

    [Add] comment

--- pkg/model/service/jwt.go
+++ pkg/model/service/jwt.go
@@ -12,5 +12,6 @@ func CreateToken(id string) (string, error) {
     claims["iat"] = time.Now()
 	claims["exp"] = time.Now().Add(time.Minute).Unix()
 	
+	// 秘密鍵作るの面倒かったから許して...
 	return token.SignedString([]byte(time.Now().String()))
 }
