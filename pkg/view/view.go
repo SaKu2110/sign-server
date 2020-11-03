9860f16d18bda1ea20b51f6fc7b05e64b0dfdddd
SaKu2110 <b1018106@fun.ac.jp>
2020-11-03 14:04:14 +0900

    [Fix] view

--- pkg/view/view.go
+++ pkg/view/view.go
@@ -5,28 +5,13 @@ import(
 	"github.com/SaKu2110/sign-server/pkg/model/dto"
 )
 
-const(
-	STATUS_CODE_400 = "Bad Request"
-	STATUS_CODE_500 = "Internal Server Error"
-)
-
-func makeErrResponse(code int, err error) *dto.Error {
-	var msg string
-	switch code {
-	case 200:
-		return nil
-	case 400:
-		msg = STATUS_CODE_400
-	case 500:
-		msg = STATUS_CODE_500
-	}
+func NewErrResponse(code int, err error) *dto.Error {
 	return &dto.Error{
 		Code: code,
-		Message: msg,
 		Description: fmt.Sprintf("%v", err),
 	}
 }
 
-func MakeSignResponse(code int, token *string, err error) dto.SignResponse {
-	return dto.SignResponse{Token: token, Err: makeErrResponse(code, err)}
+func NewSignResponse(token *string, err *dto.Error) dto.SignResponse {
+	return dto.SignResponse{Token: token, Err: err}
 }
