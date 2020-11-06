c2717c89232a50dfaad8d6bdf9e4f545545bfd8c
SaKu2110 <b1018106@fun.ac.jp>
2020-11-07 07:38:34 +0900

    [Move] jwt package

--- pkg/model/service/jwt/jwt.go
+++ pkg/model/service/jwt/jwt.go
@@ -1,17 +1,18 @@
-package service
+package jwt
 
-import(
+import (
 	"time"
+
 	jwt "github.com/dgrijalva/jwt-go"
 )
 
-func CreateJWTToken(id string) (string, error) {
+func Token(id string) (string, error) {
 	token := jwt.New(jwt.SigningMethodHS256)
 	claims := token.Claims.(jwt.MapClaims)
-    claims["id"] = id
-    claims["iat"] = time.Now()
+	claims["id"] = id
+	claims["iat"] = time.Now()
 	claims["exp"] = time.Now().Add(time.Minute).Unix()
-	
+
 	// 秘密鍵作るの面倒かったから許して...
 	return token.SignedString([]byte(time.Now().String()))
 }
