7cd818e40813856d8fb012113935e84e43a61caa
SaKu2110 <b1018106@fun.ac.jp>
2020-11-07 07:49:40 +0900

    [Add] comment

--- pkg/model/service/log/log.go
+++ pkg/model/service/log/log.go
@@ -4,14 +4,22 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+// Error 直ちに対処する必要のない実行時エラー
+func Error(msg string) {
+	log.WithFields(log.Fields{}).Error(msg)
+}
+
+// Fatal 致命的なエラー
+func Fatal(err error) {
+	log.WithFields(log.Fields{}).Fatal(err)
+}
+
+// Info 関心を寄せておきたいもの
 func Info(msg string) {
 	log.WithFields(log.Fields{}).Info(msg)
 }
 
+// Warn エラーではないが例外的なもの
 func Warn(err error) {
 	log.WithFields(log.Fields{}).Warn(err)
-}
-
-func Fatal(err error) {
-	log.WithFields(log.Fields{}).Fatal(err)
 }
