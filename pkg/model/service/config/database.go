88ce5c73c145b1f52fc2ff1f45bd8e7737a62185
SaKu2110 <b1018106@fun.ac.jp>
2020-11-07 09:49:36 +0900

    [Move] config package

---
+++ pkg/model/service/config/database.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
+type dbAccessConfig struct {
+	User string `envconfig:"DB_USER" default:"auth_user"`
+	Pass string `envconfig:"DB_PASS" default:"password"`
+	IP   string `envconfig:"DB_IP" default:"localhost"`
+	Port string `envconfig:"DB_PORT" default:"3306"`
+	Name string `envconfig:"DB_NAME" default:"auth"`
+}
+
+type dbConnConfig struct {
+	MaxOpenConns int `envconfig:"DB_MAX_CONNS" default:"100"`
+	MaxIdle      int `envconfig:"DB_MAX_IDLE" default:"100"`
+	MaxLifeTime  int `envconfig:"DB_MAX_LIFETIME" default:"100"`
+}
+
+const accessTokenTemplate = "%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local"
+
+func DBAccessToken() (string, error) {
+	var c dbAccessConfig
+	if err := envconfig.Process("", &c); err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf(accessTokenTemplate, c.User, c.Pass, c.IP, c.Port, c.Name), nil
+}
+
+func DBConnectionInfo() (int, int, time.Duration, error) {
+	var c dbConnConfig
+	if err := envconfig.Process("", &c); err != nil {
+		return 0, 0, 0, err
+	}
+
+	return c.MaxOpenConns, c.MaxIdle, time.Duration(c.MaxLifeTime) * time.Second, nil
+}
